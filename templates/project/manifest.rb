aa54478635ac8493844d2433cf90e5e7a9a692b8
unknown <kmcgill2@SCrupiSTC234.ccsunet.clayton.edu>
2014-08-15 10:13:19 -0400

    First commit

---
+++ templates/project/manifest.rb
@@ -0,0 +1,23 @@
+# Description
+description "A simple Fixed Navigation Compass extension."
+
+# Stylesheet Import
+file '_fixednav_demo.scss', :like => :stylesheet, :media => 'screen, projection'
+
+# Javascript Import
+# file 'scripts.js', :like => :javascript, :to => 'scripts.js'
+
+# General File Import
+# file 'README.md', :to => "README.md"
+
+# Compass Extension Help
+help %Q{
+  Open _fixednav_demo.scss for help and examples.
+}
+
+# Compass Extension Welcome Message
+#  Users will see this when they create a new project using this template.
+welcome_message %Q{
+  You have successfully installed the Fixed Nav demo! Open _fixednav_demo.scss
+  	for examples!
+}
